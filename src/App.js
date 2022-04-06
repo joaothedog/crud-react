09a0659016d74ea943ec94821c09b94c6839a0bf
João Oliveira <jvcbatist4@outlook.com>
2022-04-06 20:43:48 -0300

    Style in page with bulmaCss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="register container">
+        <h1 style={{ color: 'white' }} className="title is-2">iPresence</h1>
+        <section>
+          <input class="input"
+            type="text"
+            name="nome"
+            placeholder="Nome"
+            className="input register-input"
+          />
+        </section>
+        <div>
+          <input class="input"
+            type="text"
+            name="period"
+            placeholder="Período"
+            className="register-input input"
+          />
+          <input class="input"
+            type="text"
+            name="cra"
+            placeholder="Matrícula"
+            className="register-input input"
+          />
+        </div>
+        <button className="register-button button is-light is-outlined">Presente</button>
+      </div>
     </div>
   );
 }
